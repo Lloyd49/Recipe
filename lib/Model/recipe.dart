683cce1b5e9af9ea3b9a96069700a4d7f52dd175
Arthit Thongpaiboon <attpb04@gmail.com>
2025-12-12 14:03:36 +0700

    Finish

---
+++ lib/Model/recipe.dart
@@ -0,0 +1,17 @@
+class Recipe {
+  String imgLabel;
+  String imageUrl;
+
+  Recipe(this.imgLabel, this.imageUrl);
+
+  static List<Recipe> samples = [
+    Recipe('Beaf Steak', 'assets/images/beaf.webp'),
+    Recipe('Fish Curry', 'assets/images/fishcurry.webp'),
+    Recipe('Pad Thai', 'assets/images/padthai.webp'),
+    Recipe('Larb', 'assets/images/larb.webp'),
+    Recipe('Pumpkin Curry', 'assets/images/pumpkincurry.webp'),
+    Recipe('Salad', 'assets/images/salad.webp'),
+  ];
+
+}
+
